e82b78079a5a16de78767173aa6c3833ea7f7aeb
Greg <greg@avvo.com>
2016-08-02 16:30:51 -0700

    allowing additional params to come into the docker compose

--- run_test.sh
+++ run_test.sh
@@ -3,11 +3,12 @@
 set -e
 
 FILE=$1
+shift
 if [ -z "${FILE}" ]
 then
   echo "Missing docker-compose.yml file"
   exit 1
 fi
 
-docker-compose -f ${FILE} up --abort-on-container-exit
+docker-compose -f ${FILE} up --abort-on-container-exit $@
 test `docker-compose -f ${FILE} ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | grep -c -v 0` -eq 0
